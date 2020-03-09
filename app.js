fb5e942f35e6598dadbf81a38556422c25a61ce1
Your Name <you@example.com>
2020-03-09 18:29:57 -0300

    metade da aula de #40

--- app.js
+++ app.js
@@ -6,8 +6,24 @@
     const admin = require('./routers/admin')
     const path = require('path')
     const mongoose = require('mongoose')
+    const session = require('express-session')
+    const flash = require('connect-flash')
 
 //Configurações
+    //Sessão
+    app.use(session({
+        secret:'qualquercoisa',
+        resave:true,
+        saveUninitialized:true
+    }))
+    app.use(flash())
+    //Middlewate
+    app.use((req,res,next)=>{
+        //Criação de variáveis globais que serão usadas no futuro
+        res.locals.success_msg = req.flash('success_msg')
+        res.locals.error_msg=req.flash('error_msg')
+        next()
+    })
     //Body Parser
         app.use(bodyParser.urlencoded({extended:true}))
         app.use(bodyParser.json())
--- routers/admin.js
+++ routers/admin.js
@@ -22,6 +22,26 @@ router.get('/categorias/add',(req,res)=>{
 })
 
 router.post('/categorias/nova',(req,res)=>{
+    
+    //Validação do formulário
+    var erros = []
+    //verificações que não foi enviado nome, 
+    if(!req.body.nome || typeof(req.body.nome)==undefined || req.body.nome == null){
+        erros.push({text:'Nome inválido'})
+    }
+
+     //verificações que não foi enviado slug, 
+     if(!req.body.slug || typeof(req.body.slug)==undefined || req.body.slug == null){
+        erros.push({text:'Slug inválido'})
+    }
+
+    if(req.body.nome.length<2){
+        erros.push({texto:'Nome da categoria é muito pequeno'})
+    } 
+    if(erros.length>0){
+        res.render('addcategorias',{erros:erros})
+    }
+
     const novaCategoria ={
         nome:req.body.nome,
         slug:req.body.slug
