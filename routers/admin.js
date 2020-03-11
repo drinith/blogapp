ebbc25d1183263b62b43bd97dc2ca68a59f8beed
Your Name <you@example.com>
2020-03-11 18:08:01 -0300

    tentando fazer o listar

--- routers/admin.js
+++ routers/admin.js
@@ -14,7 +14,17 @@ router.get('/posts',(req,res)=>{
 })
 
 router.get('/categorias',(req,res)=>{
-    res.render('admin/categorias')
+    
+    //Listando as categorias
+
+    console.log(Categoria.find())
+    Categoria.find().then((categorias)=>{
+        res.render('admin/categorias',{categorias:categorias})
+    }).catch((err)=>{
+        req.flash('error_msg','Houve um erro ao listar as categorias')
+        res.redirect('/admin')
+    })
+    
 })
 
 router.get('/categorias/add',(req,res)=>{
